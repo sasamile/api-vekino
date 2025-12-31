d7524ae599a14509581faec87bf9f414ce75a682
Santiago Suecun <nspes2020@gmail.com>
2025-12-31 06:25:10 -0500

    fixing and push

--- src/module/plan-pricing.module.ts
+++ src/module/plan-pricing.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { PlanPricingService } from '../application/services/plan-pricing.service';
 import { PlanPricingController } from '../controllers/plan-pricing.controller';
 import { PlanPricingRepository } from '../infrastructure/repositories/plan-pricing.repository';
 import { PrismaProvider } from '../config/prisma.provider';
 import { RoleGuard } from '../config/guards/require-role.guard';
+import { CondominiosModule } from './condominios.module';
 
 @Module({
+  imports: [forwardRef(() => CondominiosModule)],
   controllers: [PlanPricingController],
   providers: [
     PlanPricingService,
